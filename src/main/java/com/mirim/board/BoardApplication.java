c88370e49ca951d4d11a4291684db7f9008c0328
oey135 <b01080126412@gmail.com>
2026-08-25 19:41:02 +0900

    시작

---
+++ src/main/java/com/mirim/board/BoardApplication.java
@@ -0,0 +1,14 @@
+package com.mirim.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication { // spring boot 앱의 시작점
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+    }
+
+}
+
