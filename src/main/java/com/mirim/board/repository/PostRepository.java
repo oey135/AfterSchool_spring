9f906eb727220fae286954e53be2ebd8aeba8e82
oey135 <b01080126412@gmail.com>
2026-09-17 18:44:20 +0900

    8일차 : PostRepository 수정

--- src/main/java/com/mirim/board/repository/PostRepository.java
+++ src/main/java/com/mirim/board/repository/PostRepository.java
@@ -10,22 +10,61 @@ import java.util.Map;
 public class PostRepository {
     // 아직 진짜 저장소는 없음
     // 컨트롤러에서 쓰던 가짜 규칙을 옮긴다. ex) id > 10이면 없다 등
+    private final list_Map posts = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public Map<String, Object> save(Map<String, Object> {
+        post.put("id, nextId++");
+        posts.add(post);
+        return post;
+    })
 
     public boolean existsById(Long id) {
-        return id <= 10;
+        return findById() != null;
     }
 
     public List<Map<String, Object>> findAll() {
         //아직 진짜 데이터는 없다.
-        return new ArrayList<>();
+        return posts;
+    }
+
+    public boolean deleteById(Long id) {
+        Map<String, Object> post = findById(id);
+        if(post == null) {
+            return false;
+        }
+        posts.remove(post);
+        return true;
+    }
+
+    public Map<String, Object> findById(Long id) {
+        //ㄱㅔ시글 전체 : posts
+        for(Map<String, Object> post : posts) {
+            if(post.get("id").equals(id)) {
+                return post;
+            }
+        }
+        return null;
     }
 
     public long count() {
-        return 0;
+        return posts.size();
     }
 
     public List<Map<String, Object>> findByKeyword(String keyword) {
-        //아직 진짜 데이터는 없다.
-        return new ArrayList<>();
+        List<Map<String, Objext>> result = new ArrayList<>();
+
+        // 1. 전체 게시글을 순회하면서 keyword 포함ㅎ는 지 확인
+        for (Map<String, Object> post : posts) {
+            String title = (String) post.get("title");
+            // 2. 포함하면 result에 게시글 정보 추가
+            if(title != null && title.contains(keyword)) {
+                result.add(post);
+            }
+            return result;
+        }
+
+
+        return result;
     }
 }
